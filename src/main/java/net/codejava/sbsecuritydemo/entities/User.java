842eb7bc1427f4dc03f803d212e55b867a99dfd8
Ruvindu Madhushanka <53707613+Ruvindu@users.noreply.github.com>
2024-01-28 21:02:13 +0530

    @Data added

--- src/main/java/net/codejava/sbsecuritydemo/entities/User.java
+++ src/main/java/net/codejava/sbsecuritydemo/entities/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Entity
 @Table(name = "users")
-//@Data
+@Data
 public class User {
 
     @Id
@@ -24,45 +24,5 @@ public class User {
 
     @Column(name = "last_name", nullable = false, length = 20)
     private String lastName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
 

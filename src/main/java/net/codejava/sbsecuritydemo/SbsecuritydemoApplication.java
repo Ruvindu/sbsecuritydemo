33799481caed7120d94ad53fe431e12e2c129696
Ruvindu Madhushanka <53707613+Ruvindu@users.noreply.github.com>
2024-01-28 15:47:48 +0530

    init spring security project

---
+++ src/main/java/net/codejava/sbsecuritydemo/SbsecuritydemoApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.sbsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecuritydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbsecuritydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/codejava/sbsecuritydemo/SbsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.sbsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsecuritydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
